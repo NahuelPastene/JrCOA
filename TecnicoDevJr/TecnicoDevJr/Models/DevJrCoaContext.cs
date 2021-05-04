810df42263acd3d8d43d63f5674188f041c99e37
Nahuel <nahuelpastene@gmail.com>
2021-05-04 11:46:18 -0300

    Agregado template boostrap + layout, agregada conexion con BD y agregado script BD (con algunos datos precargados para prueba)

---
+++ TecnicoDevJr/TecnicoDevJr/Models/DevJrCoaContext.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace TecnicoDevJr.Models
+{
+    public partial class DevJrCoaContext : DbContext
+    {
+        public DevJrCoaContext()
+        {
+        }
+
+        public DevJrCoaContext(DbContextOptions<DevJrCoaContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Usuario>(entity =>
+            {
+                entity.HasKey(e => e.IdUsuario);
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Nombre)
+                    .IsRequired()
+                    .HasMaxLength(30);
+
+                entity.Property(e => e.UserName)
+                    .IsRequired()
+                    .HasMaxLength(30);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ TecnicoDevJr/TecnicoDevJr/Models/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace TecnicoDevJr.Models
+{
+    public partial class Usuario
+    {
+        public int IdUsuario { get; set; }
+        public string UserName { get; set; }
+        public string Nombre { get; set; }
+        public string Email { get; set; }
+        public int? Telefono { get; set; }
+    }
+}
